add00f173f411f71409ef843d87eca9d54796ba0
Garbi-Collector <uwurulos@gmail.com>
2025-04-24 14:18:06 -0300

    init

---
+++ src/main/java/soundtribe/soundtribemusic/SoundtribeMusicApplication.java
@@ -0,0 +1,13 @@
+package soundtribe.soundtribemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundtribeMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundtribeMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soundtribe/soundtribemusic/SoundtribeMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package soundtribe.soundtribemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundtribeMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
