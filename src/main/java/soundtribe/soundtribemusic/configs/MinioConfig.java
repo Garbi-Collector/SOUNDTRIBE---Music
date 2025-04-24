51c63edfa76f08205d28c99525dd7f1687c4422e
Garbi-Collector <uwurulos@gmail.com>
2025-04-24 16:46:44 -0300

    init again

---
+++ src/main/java/soundtribe/soundtribemusic/configs/MinioConfig.java
@@ -0,0 +1,58 @@
+package soundtribe.soundtribemusic.configs;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.errors.MinioException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.url}")
+    private String url;
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
+    @Value("${minio.bucket-name}")
+    private String bucketName;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    public void init() {
+        try {
+            MinioClient minioClient = minioClient();
+
+            // Verifica si el bucket existe
+            boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+
+            // Si no existe, lo crea
+            if (!isExist) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+                System.out.println("Bucket '" + bucketName + "' creado exitosamente.");
+            } else {
+                System.out.println("Bucket '" + bucketName + "' ya existe.");
+            }
+
+        } catch (MinioException | InvalidKeyException | IOException | NoSuchAlgorithmException e) {
+            System.err.println("Error al verificar/crear el bucket: " + e.getMessage());
+        }
+    }
+
+}
